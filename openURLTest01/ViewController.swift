9c8456505d75598d3eb79e5cf4cf039af5581267
Masahiko Minagawa <Masahiko.Minagawa@jp.sony.com>
2018-05-02 17:59:51 +0900

    とりあえず空のTableViewを作成．

--- openURLTest01/ViewController.swift
+++ openURLTest01/ViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    let titles = ["openURL1", "openURL2", "openURL3"]
+    
     override func viewDidLoad() {
+        
+        // delegate, dataSource は Storyboard の Outlet で設定した
+        // tableView.delegate = self
+        // tableView.dataSource = self
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +29,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    // MARK: UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count;
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("indexPath.row=\(indexPath.row)")
+    }
+    
+    
+    // MARK: UITableViewDataSource
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print("indexPath.row=\(indexPath.row)")
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TopTableViewCellID", for: indexPath)
+        
+        cell.textLabel?.text = titles[indexPath.row]
+        
+        return cell;
+    }
+    
 }
-
